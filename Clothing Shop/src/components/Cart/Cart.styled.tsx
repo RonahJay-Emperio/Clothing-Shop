e2ec10d94266c1eeb119e2248aec2f00bb933da5
Ronah Jay Emperio <ronahjay.emperio.20@usjr.edu.ph>
2022-12-24 15:07:47 +0800

    update card component

--- Clothing Shop/src/components/Cart/Cart.styled.tsx
+++ Clothing Shop/src/components/Cart/Cart.styled.tsx
@@ -12,3 +12,21 @@ export const ProductsWrapper = styled.div`
   grid-template-columns: repeat(2, auto);
   gap: 20px;
 `;
+
+export const Header = styled.div`
+
+  width:500px;
+  display: flex;
+  justify-content: space-between;
+  
+`;
+export const LinksWrapper = styled.div`
+
+
+  a {
+    text-decoration: none;
+    color: red;
+
+  }
+
+`;
--- Clothing Shop/src/components/Cart/Cart.tsx
+++ Clothing Shop/src/components/Cart/Cart.tsx
@@ -2,13 +2,17 @@ import { useContext } from "react";
 import { ClothingShopContext, } from "../../useContext/shopContext";
 import { Product } from "../../models";
 import { ProductCard } from "../ProductCard";
-import { ProductsWrapper, Title } from "./Cart.styled";
+import { ProductsWrapper, Title, Header, LinksWrapper } from "./Cart.styled";
 
+import { Link } from "react-router-dom";
 export const Cart = () => {
   const { products, total } = useContext(ClothingShopContext);
   return (
     <>
-      <Title>Your cart total is {total}.00$</Title>
+      <Header>
+        <Title>Your cart total is {total}.00$</Title>
+        <Link to="/checkout" >Checkout</Link>
+      </Header>
       <ProductsWrapper>
         {products.map((product: Product, index) => (
           <ProductCard {...product} key={index} />
